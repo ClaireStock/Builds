0c364a81418c3d7c2ca97d4260280eb0d1653e01
ClaireStock <97408872+ClaireStock@users.noreply.github.com>
2022-01-09 20:15:18 +0000

    Add files via upload

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Bob";
+int message = 3;
+decimal temp = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(message);
+Console.Write(" in your inbox. ");
+Console.Write("The temperature is ");
+Console.Write(temp);
+Console.Write(" celsius.");
